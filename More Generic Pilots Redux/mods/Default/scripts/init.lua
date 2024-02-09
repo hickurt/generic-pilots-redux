24de08a95307c1921c9746b5884e14c8f08af19d
hickurt <157117730+hickurt@users.noreply.github.com>
2024-02-09 04:19:33 +0000

    Add files via upload

--- More Generic Pilots Redux/mods/Default/scripts/init.lua
+++ More Generic Pilots Redux/mods/Default/scripts/init.lua
@@ -2,7 +2,7 @@ return {
 	id = "more_generic_default",
 	name = "Default",
 	version = "1.0",
-	description = "Default version with all pilots + toggles.",
+	description = "Default version with all pilots and variations.",
 	icon = "img/icon.png",
 	submodFolders = {"mods/"},
 	init = function() end,
--- More Generic Pilots Redux/mods/Streamlined/scripts/init.lua
+++ More Generic Pilots Redux/mods/Streamlined/scripts/init.lua
@@ -2,7 +2,7 @@ return {
 	id = "more_generic_streamlined",
 	name = "Streamlined",
 	version = "1.0",
-	description = "A version more in line with vanilla, adding only the new portraits, no variations.",
+	description = "A version more in line with vanilla, adding only the new pilots, no variations.",
 	icon = "img/icon.png",
 	submodFolders = {"mods/"},
 	enabled = false,
